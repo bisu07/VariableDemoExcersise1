bc4aadb3b762c671a0ab7025e58f657f06d616d5
bisu07 <g26.bishal@gmail.com>
2021-09-27 12:33:31 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariableDemoExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Denny";
+            int hisAge = 44;
+            char hisGender = 'm';
+            bool isEmployeed = true;
+            double hisSalary = 5000;
+            decimal hisHeight = 170;
+            Console.WriteLine($"My friend name is {firstName},he is {hisAge} years old, his gender is {hisGender},he is employeed {isEmployeed}, he got monthly salary {hisSalary} and he is about {hisHeight} cm tall");
+
+        }
+    }
+}
